2bb152826786f64647ee1296d1fa32402fcda609
rusta <rustekapl@gmail.com>
2022-07-02 14:14:36 +0200

    clear Runner

--- src/main/java/ru/javarush/island/aleev/entity/map/GameMap.java
+++ src/main/java/ru/javarush/island/aleev/entity/map/GameMap.java
@@ -16,16 +16,13 @@ import java.util.*;
 public class GameMap {
 
 
-//    public final Cell[][] cells = new Cell[2][2];
+
 
     public static final int rows = Constants.WIDTH_ISLAND;
     public static final int colls = Constants.LENGTH_ISLAND;
 
     public static Cell[][] cells=new Cell[rows][colls];
 
-//    public GameMap(int rows, int colls) {
-//        this.cells = new Cell[rows][colls];
-//    }
 
 
     //создаем массив ячеек игрового поля
@@ -110,80 +107,6 @@ public void getNextCells(){
         }
     }
 
-//    public void fill() {
-//        for (Cell[] cell : cells) {
-//            for (Cell item : cell) {
-//
-//
-//                //Делаем сет медведей
-//
-//
-//                Set<Organism> setBears = new HashSet<>();
-//                countBear = Randomizer.get(2, Constants.MAX_COUNT_BEAR);
-//                for (int k = 0; k < countBear; k++) {
-//                    setBears.add(new Bear(new Parameters(Constants.NAME_BEAR, Constants.BEAR_ICON,
-//                            Randomizer.get(Constants.MAX_WEIGHT_BEAR - Constants.MAX_FOOD_BEAR, Constants.MAX_WEIGHT_BEAR),
-//                            Constants.MAX_WEIGHT_BEAR, Constants.MAX_COUNT_BEAR, Constants.MAX_SPEED_BEAR, Constants.MAX_FOOD_BEAR)));
-//                }
-//                System.out.println(setBears);
-//                item.residents.put(Bear.class.getSimpleName(), setBears);
-//
-//                //Делаем сет волков
-//                Set<Organism> setWolves = new HashSet<>();
-//                countWolf = Randomizer.get(2, Constants.MAX_COUNT_WOLF);
-//                for (int k = 0; k < countWolf; k++) {
-//                    setWolves.add(new Wolf(new Parameters(Constants.NAME_WOLF, Constants.WOLF_ICON,
-//                            Randomizer.get(Constants.MAX_WEIGHT_WOLF - Constants.MAX_FOOD_WOLF, Constants.MAX_WEIGHT_WOLF),
-//                            Constants.MAX_WEIGHT_WOLF, Constants.MAX_COUNT_WOLF, Constants.MAX_SPEED_WOLF, Constants.MAX_FOOD_WOLF)));
-//                }
-//                System.out.println(setWolves);
-//                item.residents.put(Wolf.class.getSimpleName(), setWolves);
-//
-//                //Делаем сет кабанов
-//                Set<Organism> setBoars = new HashSet<>();
-//                countBoar = Randomizer.get(2, Constants.MAX_COUNT_BOAR);
-//                for (int k = 0; k < countBoar; k++) {
-//                    setBoars.add(new Boar(new Parameters(Constants.NAME_BOAR, Constants.BOAR_ICON,
-//                            Randomizer.get(Constants.MAX_WEIGHT_BOAR - Constants.MAX_FOOD_BOAR, Constants.MAX_WEIGHT_BOAR),
-//                            Constants.MAX_WEIGHT_BOAR, Constants.MAX_COUNT_BOAR, Constants.MAX_SPEED_BOAR, Constants.MAX_FOOD_BOAR)));
-//                }
-//                System.out.println(setBoars);
-//                item.residents.put(Boar.class.getSimpleName(), setBoars);
-//
-//                //Делаем сет лошадей
-//                Set<Organism> setHorses = new HashSet<>();
-//                countHorse = Randomizer.get(2, Constants.MAX_COUNT_HORSE);
-//                for (int k = 0; k < countHorse; k++) {
-//                    setHorses.add(new Horse(new Parameters(Constants.NAME_HORSE, Constants.HORSE_ICON,
-//                            Randomizer.get(Constants.MAX_WEIGHT_HORSE - Constants.MAX_FOOD_HORSE, Constants.MAX_WEIGHT_HORSE),
-//                            Constants.MAX_WEIGHT_HORSE, Constants.MAX_COUNT_HORSE, Constants.MAX_SPEED_HORSE, Constants.MAX_FOOD_HORSE)));
-//                }
-//                System.out.println(setHorses);
-//                item.residents.put(Horse.class.getSimpleName(), setHorses);
-//
-//                //Делаем сет растений
-//                Set<Organism> setPlants = new HashSet<>();
-//                for (int k = 0; k < Constants.MAX_COUNT_PLANT; k++) {
-//                    setPlants.add(new Plant(new Parameters(Constants.NAME_PLANT, Constants.PLANT_ICON,
-//                            Constants.MAX_WEIGHT_PLANT, Constants.MAX_WEIGHT_PLANT, Constants.MAX_COUNT_PLANT, 0, 0)));
-//
-//                }
-//                System.out.println(setPlants);
-//                item.residents.put(Plant.class.getSimpleName(), setPlants);
-//
-//
-//                System.out.println("-----------------------------------------------------------------------------");
-//
-//
-//                item.residents.forEach((key, value) -> System.out.print(key + "=" + value.size() + " "));
-//                System.out.println("\n");
-//                System.out.println("=============================================================================");
-//
-//
-//
-//            }
-//        }
-//    }
 
     // вывод на экран карты
 //   TODO разобрать
@@ -204,14 +127,7 @@ public void getNextCells(){
 //        }
 //    }
 
-//    public void print() {
-//        for (Cell[] cell : cells) {
-//            for (Cell value : cell) {
-//                System.out.print(value);
-//            }
-//            System.out.println();
-//        }
-//    }
+
 
     public void printInfo() {
         Map<String, Integer> map = new HashMap<>();
@@ -223,16 +139,16 @@ public void getNextCells(){
                         .stream()
                         .filter(s -> s.size() > 0)
                         .forEach(s -> map.put(s.stream().findFirst().get().toString(), s.size()));
-                System.out.print(map);
-                System.out.print("|\t");
+                System.out.println("\t|" + cell.getRow() + "/" + cell.getCol() + "|\t"+map);
+                //System.out.print("|\t");
             }
             System.out.println();
         }
         int totalCount = 0;
 
-        for (int i = 0; i < cells.length; i++) {
-            for (int j = 0; j < cells[i].length; j++) {
-                Map<Type, Set<Organism>> map1 = cells[i][j].resident;
+        for (Cell[] cell : cells) {
+            for (int j = 0; j < cell.length; j++) {
+                Map<Type, Set<Organism>> map1 = cell[j].resident;
                 for (Set<Organism> value : map1.values()) {
                     for (Organism alive : value) {
                         totalCount++;
@@ -243,17 +159,7 @@ public void getNextCells(){
         System.out.println("Total value of Organisms are  " + totalCount);
     }
 
-//    public void printInfo() {
-//        for (Cell[] cell : cells) {
-//            for (Cell item : cell) {
-//                System.out.print(item + "\t");
-//
-//                item.getOrganisms().forEach((key, value) -> System.out.print(key + "=" + value.size() + " "));
-//                System.out.println("\n");
-//
-//            }
-//        }
-//    }
+
 
     public Cell[][] getCells() {
         return cells;
